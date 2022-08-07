957269bda53b8f66c769b908d41a8281fdac7a4d
SNderi <nderisharonw@gmail.com>
2022-08-07 13:06:39 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,22 +71,3 @@ class FileStorage:
                     FileStorage.__objects[key] = Place(**deserialize[key])
                 elif deserialize[key]['__class__'] == "Review":
                     FileStorage.__objects[key] = Review(**deserialize[key])
-
-    def classes(self):
-        """ Returns a dictionary of valid classes and their references. """
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
